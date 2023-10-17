b9c9bfbc3b1f4f16d88af5aed1e5ae802442ed89
pccommen <pccommen@gmail.com>
2023-10-17 22:21:34 +0900

    feat: AuthView & AuthViewController 구현 #15

---
+++ QED/Presentation/Auth/AuthViewController.swift .swift
@@ -0,0 +1,93 @@
+//
+//  AuthViewController.swift
+//  QED
+//
+//  Created by changgyo seo on 10/17/23.
+//
+
+import SwiftUI
+
+import FirebaseCore
+import FirebaseAuth
+import GoogleSignIn
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        layout()
+    }
+
+    // MARK: UI
+    lazy var googleSignInView: UIView = {
+        let view = UIView()
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(tapGoogleSignInView(_:)))
+
+        view.backgroundColor = .red
+        view.addGestureRecognizer(gesture)
+
+        return view
+    }()
+
+    lazy var kakaoSignInview: UIView = {
+        let view = UIView()
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(tapKakaoSignInView(_:)))
+
+        view.backgroundColor = .blue
+        view.addGestureRecognizer(gesture)
+
+        return view
+    }()
+
+    lazy var appleSignInView: UIView = {
+        let view = UIView()
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(tapAppleSignInView(_:)))
+
+        view.backgroundColor = .green
+        view.addGestureRecognizer(gesture)
+
+        return view
+    }()
+
+    // MARK: AutoConstraint
+    func layout() {
+        [googleSignInView, kakaoSignInview, appleSignInView].forEach {
+            view.addSubview($0)
+        }
+
+        googleSignInView.translatesAutoresizingMaskIntoConstraints = false
+        googleSignInView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        googleSignInView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        googleSignInView.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        googleSignInView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+
+        kakaoSignInview.translatesAutoresizingMaskIntoConstraints = false
+        kakaoSignInview.topAnchor.constraint(equalTo: googleSignInView.bottomAnchor, constant: 30).isActive = true
+        kakaoSignInview.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        kakaoSignInview.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        kakaoSignInview.heightAnchor.constraint(equalToConstant: 100).isActive = true
+
+        appleSignInView.translatesAutoresizingMaskIntoConstraints = false
+        appleSignInView.topAnchor.constraint(equalTo: kakaoSignInview.bottomAnchor, constant: 30).isActive = true
+        appleSignInView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        appleSignInView.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        appleSignInView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+    }
+
+    // MARK: Auth Logic
+    private let clientID = FirebaseApp.app()?.options.clientID
+
+    @objc
+    func tapGoogleSignInView(_ sender: Any) {
+    }
+
+    @objc
+    func tapKakaoSignInView(_ sender: Any) {
+
+    }
+
+    @objc
+    func tapAppleSignInView(_ sender: Any) {
+
+    }
+}
