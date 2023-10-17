2842de619124834d1b9755b022d53a149235e369
OLING <yealin9399@gmail.com>
2023-10-18 04:48:14 +0900

    feat: PerformanceSettingViewModel headcount 증가, 감소 기능 추가#11

--- QED/Presentation/PerformanceSetting/ViewModel/PerformanceSettingViewModel.swift
+++ QED/Presentation/PerformanceSetting/ViewModel/PerformanceSettingViewModel.swift
@@ -8,7 +8,17 @@
 import Foundation
 
 struct PerformanceSettingViewModel {
-    
-    var search = ""
+    var headcount = 2
 
+    mutating func decreaseHeadcount() {
+        if headcount > 2 {
+            headcount -= 1
+        }
+    }
+
+    mutating func increaseHeadcount() {
+        if headcount < 13 {
+            headcount += 1
+        }
+    }
 }
