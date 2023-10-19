a96516feed59da7a8a9a293c32c9a5dc6453469f
pccommen <pccommen@gmail.com>
2023-10-19 21:04:23 +0900

    feat: Auth관련 Repository protocol 구현 #15

---
+++ QED/Domain/Repositories/AuthRepository/AppleAuthRepository.swift
@@ -0,0 +1,12 @@
+//
+//  AppleAuthRepository.swift
+//  QED
+//
+//  Created by changgyo seo on 10/19/23.
+//
+
+import Foundation
+
+protocol AppleAuthRepository: AuthRepository {
+    
+}
---
+++ QED/Domain/Repositories/AuthRepository/KakaoAuthRepository.swift
@@ -0,0 +1,12 @@
+//
+//  KakaoAuthRepository.swift
+//  QED
+//
+//  Created by changgyo seo on 10/19/23.
+//
+
+import Foundation
+
+protocol KakaoAuthRepository: AuthRepository {
+    
+}
