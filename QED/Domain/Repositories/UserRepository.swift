8b92471f0d678f75edb057b0f52bf78ed98f0548
pccommen <pccommen@gmail.com>
2023-10-20 15:26:22 +0900

    feat: UserRepository readUser 파라미터 id: Int로 변경 #30

--- QED/Domain/Repositories/UserRepository.swift
+++ QED/Domain/Repositories/UserRepository.swift
@@ -4,6 +4,6 @@ import Foundation
 
 protocol UserRepository {
     func createUser(_ user: User) async throws -> User
-    func readUser(email: String) async throws -> User
+    func readUser(id: Int) async throws -> User
     func updateUser(_ user: User) async throws -> User
 }
