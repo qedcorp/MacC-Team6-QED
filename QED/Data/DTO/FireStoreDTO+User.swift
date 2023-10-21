4878f533a649c7e3fe70e8648ff7ba4f3ed22d54
pccommen <pccommen@gmail.com>
2023-10-21 23:25:30 +0900

    feat: User DTO 구현 #30

---
+++ QED/Data/DTO/FireStoreDTO+User.swift
@@ -0,0 +1,37 @@
+//
+//  FireStoreDTO+User.swift
+//  QED
+//
+//  Created by changgyo seo on 10/21/23.
+//
+
+import Foundation
+
+extension FireStoreDTO {
+    class User: NSObject, EnableFirestore {
+        let collectionName: String = "USER"
+        @objc var ID: String
+        @objc var EMAIL: String?
+        @objc var NAME: String?
+        
+        init(EMAIL: String?, NAME: String?) {
+            self.ID = ""
+            self.EMAIL = EMAIL
+            self.NAME = NAME
+        }
+        
+        convenience override init() {
+            self.init(EMAIL: "", NAME: "")
+        }
+    }
+}
+
+extension FireStoreDTO.User {
+    var entity: User {
+        User(
+            id: ID,
+            email: EMAIL,
+            nickname: NAME
+        )
+    }
+}
