72acc55407705d99738b46e6dc936f8f79fe56bd
pccommen <pccommen@gmail.com>
2023-10-27 23:46:05 +0900

    feat: MusicRepository 에 함수추가 #57

--- QED/Domain/Repositories/MusicRepository.swift
+++ QED/Domain/Repositories/MusicRepository.swift
@@ -5,4 +5,5 @@ import Foundation
 protocol MusicRepository {
     func readMusic(id: String) async throws -> Music
     func readMusics(keyword: String) async throws -> [Music]
+    func readLyrics(title: String, artist: String) async throws -> Music.Lyric
 }
