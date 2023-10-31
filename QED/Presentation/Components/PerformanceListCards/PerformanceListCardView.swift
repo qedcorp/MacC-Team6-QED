ede204b0911be7b383d929e46ce413cadcc2e9f0
chaekie <chaekie.kim@gmail.com>
2023-11-01 02:42:36 +0900

    feat: 로딩스피너 추가 #75

--- QED/Presentation/Components/PerformanceListCards/PerformanceListCardView.swift
+++ QED/Presentation/Components/PerformanceListCards/PerformanceListCardView.swift
@@ -13,6 +13,7 @@ struct PerformanceListCardView: View {
     var creator: String
     var albumCoverURL: URL?
     var image: UIImage?
+    @State private var isLoading = true
 
     init(performance: PerformanceModel) {
         self.performance = performance
@@ -23,24 +24,39 @@ struct PerformanceListCardView: View {
 
     var body: some View {
         VStack(alignment: .leading) {
-            AsyncImage(url: performance.music.albumCoverURL) { image in
-                image
-                    .image?.resizable()
-                    .scaledToFill()
+            AsyncImage(url: performance.music.albumCoverURL) { phase in
+                switch phase {
+                case .empty:
+                    VStack {
+                        Spacer()
+                        ProgressView()
+                        Spacer()
+                    }
+                case .success(let image):
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .onAppear {
+                            isLoading = false
+                        }
+                case .failure:
+                    Image(systemName: "exclamationmark.circle.fill")
+                @unknown default:
+                    Image(systemName: "exclamationmark.circle.fill")
+                }
             }
-            VStack(alignment: .leading) {
-                Text("\(performance.music.title)")
-                    .font(.system(size: 13))
-                    .bold()
-                    .foregroundColor(Color.black)
-                    .opacity(0.8)
-
-                Text("\(creator)")
-                    .font(.system(size: 11))
-                    .foregroundColor(Color.black)
-                    .opacity(0.6)
+            if !isLoading {
+                VStack(alignment: .leading) {
+                    Text("\(performance.music.title)")
+                        .font(.footnote)
+                        .bold()
+                        .opacity(0.8)
+                    Text("\(creator)")
+                        .font(.caption2)
+                        .opacity(0.6)
+                }
+                .padding(.horizontal)
             }
-            .padding(.horizontal)
 
             Spacer()
         }
