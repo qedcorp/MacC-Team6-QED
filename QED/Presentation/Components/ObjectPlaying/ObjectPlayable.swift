17b34f60322ae36d164164d501b74825f85d34c6
pccommen <pccommen@gmail.com>
2023-11-02 20:10:58 +0900

    feat: Add ObjectPlayable

---
+++ QED/Presentation/Components/ObjectPlaying/ObjectPlayable.swift
@@ -0,0 +1,33 @@
+//
+//  ObjectPlayable.swift
+//  QED
+//
+//  Created by changgyo seo on 11/2/23.
+//
+
+import UIKit
+
+class ObjectPlayable: ObjectStageViewController {
+    
+    var index: Int
+
+    init(index: Int) {
+        self.index = index
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    override func loadView() {
+        super.loadView()
+        setupCenterLines()
+    }
+    
+    private func setupCenterLines() {
+        let renderer = CenterLinesRenderer(color: .green)
+        renderer.render(in: view)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
