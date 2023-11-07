d6ba2d140654bd474ff23f1f46180015843dc5a7
pccommen <pccommen@gmail.com>
2023-11-07 22:04:54 +0900

    feat: MainView @main에 걸기 #104

--- QED/Presentation/QEDApp.swift
+++ QED/Presentation/QEDApp.swift
@@ -9,18 +9,12 @@ import Combine
 struct QEDApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @State var isLogin = false
-    
+
     var body: some Scene {
         WindowGroup {
             if isLogin || (try? KeyChainManager.shared.read(account: .id)) != nil {
                 NavigationView {
-                    FormationSettingView(
-                        performance: Performance(id: "", author: .sample, music: .newJeans, headcount: 5),
-                        performanceUseCase: DefaultPerformanceUseCase(
-                            performanceRepository: MockPerformanceRepository(),
-                            userStore: DefaultUserStore.shared
-                        )
-                    )
+                    MainView()
                 }
             } else {
                 AuthView(loginViewModel: LoginViewModel(isLogin: $isLogin))
