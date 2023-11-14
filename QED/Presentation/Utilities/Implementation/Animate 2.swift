f4e576d3dea53d46eab11edb6d78d09121462a8f
OLING <yealin9399@gmail.com>
2023-11-15 00:12:56 +0900

    feat: 겹친 파일 삭제#101

--- QED/Presentation/Utilities/Implementation/Animate 2.swift
+++
@@ -1,7 +0,0 @@
-// Created by byo.
-
-import SwiftUI
-
-func animate<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
-    try withAnimation(animation, body)
-}
