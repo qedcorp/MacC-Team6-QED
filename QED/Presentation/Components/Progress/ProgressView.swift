0e2b040ec4f68a087012dee9096a2bda5b782d0e
OLING <yealin9399@gmail.com>
2023-11-15 16:58:15 +0900

    feat: progressComponent #101

---
+++ QED/Presentation/Components/Progress/ProgressView.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressView.swift
+//  QED
+//
+//  Created by OLING on 11/15/23.
+//
+
+import SwiftUI
+
+struct ProgressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressView()
+}
