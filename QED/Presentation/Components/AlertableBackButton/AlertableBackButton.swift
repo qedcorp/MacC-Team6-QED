e386c88c5c8f8a54e9c385842d57a1f3ceabbf13
Taehyun <taehyunjo97@gmail.com>
2023-11-23 22:49:56 +0900

    feat: 되돌아가기 텍스트 반영 #197

--- QED/Presentation/Components/AlertableBackButton/AlertableBackButton.swift
+++ QED/Presentation/Components/AlertableBackButton/AlertableBackButton.swift
@@ -12,7 +12,7 @@ struct AlertableBackButton: View {
             case .home:
                 return "홈으로 나가기"
             case .back:
-                return "뒤로가기"
+                return "되돌아가기"
             }
         }
 
@@ -21,7 +21,7 @@ struct AlertableBackButton: View {
             case .home:
                 return "지금까지 작성한 프로젝트 내용이 모두 삭제됩니다. 홈으로 나가시겠어요?"
             case .back:
-                return "ㄹㅇㄱ?"
+                return "보기로 되돌아 갈 경우\n수정사항은 반영되지 않습니다.\n정말 되돌아가시겠어요?"
             }
         }
     }
@@ -43,8 +43,8 @@ struct AlertableBackButton: View {
         }
         .navigationBarBackButtonHidden()
         .alert(alert.title, isPresented: $isAlertPresented, actions: {
-            Button("아니오", role: .cancel) {}
-            Button("네", role: .destructive) { dismiss() }
+            Button("취소", role: .cancel) {}
+            Button("확인", role: .destructive) { dismiss() }
         }) {
             Text(alert.content)
         }
