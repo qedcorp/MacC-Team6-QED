e216187f2610152dcbac2316dc420695902d8ca8
pccommen <pccommen@gmail.com>
2024-01-13 01:08:51 +0900

    feat: 개선의견남기기alert 개발 및 Keychain에 LaunchingCount 추가 #219

--- QED/Domain/Stores/Default/DefaultUserStore.swift
+++ QED/Domain/Stores/Default/DefaultUserStore.swift
@@ -12,13 +12,33 @@ final class DefaultUserStore: UserStore {
         let email = try? KeyChainManager.shared.read(account: .email)
         let nickname = try? KeyChainManager.shared.read(account: .name)
         let launchingCountString = try? KeyChainManager.shared.read(account: .launchingCount)
-        let launchingCount = launchingCountString == nil ? 0 : Int(launchingCountString!)
-        
-        User(id: id ?? "",
+        let launchingCount = launchingCountString == nil ? 0 : Int(launchingCountString!)!
+
+        myUser = User(id: id ?? "",
              email: email,
              nickname: nickname,
              isNotificationOn: nil,
              launchingCount: launchingCount
         )
+    }
+
+    @discardableResult
+    func increaseLaunchingCount() -> Int {
+        guard let myUser = myUser else { return 0 }
+        if myUser.launchingCount >= 0 {
+            myUser.launchingCount += 1
+            try? KeyChainManager.shared.delete(account: .launchingCount)
+            try? KeyChainManager.shared.create(account: .launchingCount, data: String(myUser.launchingCount))
+            return myUser.launchingCount
+        } else {
+            return myUser.launchingCount
+        }
+    }
+
+    func resetLaunchingCount() {
+        guard let myUser = myUser else { return }
+        myUser.launchingCount = -1
+        try? KeyChainManager.shared.delete(account: .launchingCount)
+        try? KeyChainManager.shared.create(account: .launchingCount, data: String(myUser.launchingCount))
     }
 }
